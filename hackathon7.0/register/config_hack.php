a26feb0f1227753a1598d17f785f9aacaef7dfdc
sjain7787 <90253976+sjain7787@users.noreply.github.com>
2023-07-19 11:46:11 +0530

    removed files

--- hackathon7.0/register/config_hack.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","upescit8_upescsi","UPESCSI@2021s")or die("Could not connect to MYSQL");
-mysqli_select_db($con,'upescit8_hackathon7') or die("Could not select db");
-?>
-
-
